f9f3b72d1468201c86f55f8868bf6a56c8c7d33e
wongkh <wongkh@usc.edu>
2019-12-16 20:55:31 -0800

    initial commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
+import Typist from "react-typist";
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div class="mainDiv" align="center">
+          <link rel="stylesheet" href="home.css" />
+          <Typist>
+            <p class="bio">
+              Hi! My name is Katherine Wong and I am a software engineer in the
+              San Francisco Area.
+            </p>
+          </Typist>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+export default Home;
---
+++ src/pages/work.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./work.css";
+class Work extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div class="mainDiv">
+          <link rel="stylesheet" href="work.css" />
+          <iframe
+            class="frame"
+            src="https://www.slideshare.net/slideshow/embed_code/key/ldxkO6woqJgUe2"
+          ></iframe>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+export default Work;

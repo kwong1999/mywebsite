afb85b0f9fc83f0621e4112a58419a32559f8353
wongkh <wongkh@usc.edu>
2019-12-16 21:40:16 -0800

    add url

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
         <div>
           <Route
             exact={true}

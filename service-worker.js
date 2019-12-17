1e086efeb8fab2931e7f84094fcfd2ec6b9f091c
wongkh <wongkh@usc.edu>
2019-12-17 13:48:31 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mywebsite/precache-manifest.892a761fb5784d5668c5d8d0c142391e.js"
+  "/mywebsite/precache-manifest.fdf3414fa57fe1fcaa76b5aefcbe43d0.js"
 );
 
 self.addEventListener('message', (event) => {
